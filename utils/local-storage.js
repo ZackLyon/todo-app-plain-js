4e0a382c18603cdc5117e91dd02424effce06c30
Zack Lyon <zack.j.lyon@gmail.com>
2021-09-08 15:16:48 -0700

    Add sign up page with set user function

--- utils/local-storage.js
+++ utils/local-storage.js
@@ -1,2 +1,8 @@
-// export { setUser };
+export { setUser };
 
+const USER = 'USER';
+
+function setUser(userObj){
+    const stringUser = JSON.stringify(userObj);
+    localStorage.setItem(USER, stringUser);
+}
