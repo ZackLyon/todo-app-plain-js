b57eeb799021459af701ab5c5851f36292548984
Zack Lyon <zack.j.lyon@gmail.com>
2021-09-08 16:32:54 -0700

    Add login page and getUser function

--- utils/local-storage.js
+++ utils/local-storage.js
@@ -1,8 +1,12 @@
-export { setUser };
+export { setUser, getUser };
 
 const USER = 'USER';
 
 function setUser(userObj){
     const stringUser = JSON.stringify(userObj);
     localStorage.setItem(USER, stringUser);
+}
+
+function getUser(){
+    return JSON.parse(localStorage.getItem(USER));
 }
